a7e1d63f1320152d10201948fa4e7040e91463a6
Abuzar Karimzada <64160559+Abuzar-cpu@users.noreply.github.com>
2021-12-27 16:54:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 let chances = 10;
 let won = false;
+let lost = false;
 let random = Math.ceil(Math.random() * 10);
 document.getElementById("remaining").innerHTML = "Remainning chances: " + chances;
 chances--;
+
+function userWon() {
+
+    document.getElementById('computer').innerHTML = random;
+    document.getElementById("remaining").style.backgroundColor = "lime";
+    document.getElementById("remaining").innerHTML = "YOU WON!";
+    won = true;
+    alert("Congrats");
+}
+
+function userLost() {
+    lost = true;
+    document.getElementById("remaining").innerHTML = "YOU LOST";
+}
+
+function reset() {
+    random = Math.ceil(Math.random() * 10);
+    chances = 10;
+    document.getElementById("remaining").style.backgroundColor = "red";
+    won = false;
+    lost = false;
+}
+
+
 window.addEventListener('keydown', function (e) {
     if (isFinite(e.key)) {
-        if (won === true) {
-            random = Math.ceil(Math.random() * 10);
-            chances = 10;
+        if (won === true || lost === true) {
             reset();
-            won = false;
         }
 
         document.getElementById("remaining").innerHTML = "Remainning chances: " + chances;
         if (chances > 0) {
             document.getElementById("your").innerHTML = e.key;
             if (e.key == random) {
-                document.getElementById('computer').innerHTML = random;
-                document.getElementById("remaining").style.backgroundColor = "lime";
-                document.getElementById("remaining").innerHTML = "YOU WON!";
-                won = true;
-                alert("Congrats");
+                userWon();
             }
             else {
                 chances -= 1;
             }
         }
         else {
-            won = true;
-            document.getElementById("remaining").innerHTML = "YOU LOST";
+            userLost();
         }
     }
 
 })
-
-
-function reset() {
-    document.getElementById("remaining").style.backgroundColor = "red";
-}
