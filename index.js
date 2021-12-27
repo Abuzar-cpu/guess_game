6b2f4fe8871fb0ca25306ffd53df0ee4c2f63868
Abuzar Karimzada <64160559+Abuzar-cpu@users.noreply.github.com>
2021-12-27 16:48:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,38 @@ let won = false;
 let random = Math.ceil(Math.random() * 10);
 document.getElementById("remaining").innerHTML = "Remainning chances: " + chances;
 chances--;
-window.addEventListener('keydown', function(e){
-    if(won === true)
-    {
-        random = Math.ceil(Math.random() * 10);
-        chances = 10;
-        reset();
-        won = false;
+window.addEventListener('keydown', function (e) {
+    if (isFinite(e.key)) {
+        if (won === true) {
+            random = Math.ceil(Math.random() * 10);
+            chances = 10;
+            reset();
+            won = false;
+        }
+
+        document.getElementById("remaining").innerHTML = "Remainning chances: " + chances;
+        if (chances > 0) {
+            document.getElementById("your").innerHTML = e.key;
+            if (e.key == random) {
+                document.getElementById('computer').innerHTML = random;
+                document.getElementById("remaining").style.backgroundColor = "lime";
+                document.getElementById("remaining").innerHTML = "YOU WON!";
+                won = true;
+                alert("Congrats");
+            }
+            else {
+                chances -= 1;
+            }
+        }
+        else {
+            won = true;
+            document.getElementById("remaining").innerHTML = "YOU LOST";
+        }
     }
 
-    document.getElementById("remaining").innerHTML = "Remainning chances: " + chances;
-    if(chances > 0)
-    {
-        document.getElementById("your").innerHTML = e.key;        
-        if (e.key == random)
-        {
-            document.getElementById('computer').innerHTML = random;
-            document.getElementById("remaining").style.backgroundColor  = "lime";
-            document.getElementById("remaining").innerHTML = "YOU WON!";
-            won = true;
-            alert("Congrats");
-        }
-        else{
-            chances -= 1;
-        }
-    }
-    else{
-        won = true;
-        document.getElementById("remaining").innerHTML = "YOU LOST";
-    }
-    
 })
 
 
 function reset() {
-    document.getElementById("remaining").style.backgroundColor  = "red";
+    document.getElementById("remaining").style.backgroundColor = "red";
 }
